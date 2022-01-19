c6021501451efc2270c3d8658824063a8d50e8be
Piotr Smietanka <piotrsm99@gmail.com>
2022-01-19 15:29:47 +0100

    feat: react-scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,31 @@
 import "./Navbar.scss";
 import { useState, useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Link as Scroll, Element, animateScroll } from "react-scroll";
 
 export default function Navbar(params) {
-  const home = useRef();
-  const about = useRef();
-  const passions = useRef();
-  const portfolio = useRef();
-
-  const [lastClicked, setLastClicked] = useState("");
-
-  const toggle = (e) => {
-    if (lastClicked) lastClicked.style.color = "white";
-    if (!lastClicked) {
-      setLastClicked(e.target);
-    }
-
-    e.target.style.color = "#dc2e2e";
-
-    setLastClicked(e.target);
-  };
-
-  useEffect(() => {
-    home.current.style.color = "#dc2e2e";
-    setLastClicked(home.current);
-  }, []);
-
   return (
-    <div className="navbar_container">
+    <nav className="navbar_container">
       <div className="navbar_left">
-        <a ref={home} href="#home" onClick={(e) => toggle(e)}>
+        <Scroll to="home" className="home">
           Home
-        </a>
-        <a ref={about} href="#about" onClick={(e) => toggle(e)}>
+        </Scroll>
+
+        <Scroll to="about" className="about">
           About
-        </a>
-        <a ref={passions} href="#passions" onClick={(e) => toggle(e)}>
+        </Scroll>
+
+        <Scroll to="passions" className="passions">
           Passions
-        </a>
-        <a ref={portfolio} href="#portfolio" onClick={(e) => toggle(e)}>
+        </Scroll>
+
+        <Scroll to="portfolio" className="portfolio">
           Portfolio
-        </a>
+        </Scroll>
       </div>
       <div className="navbar_right">
-        <a href="#contact">Contact Me</a>
+        <Scroll to="contact">Contact Me</Scroll>
       </div>
-    </div>
+    </nav>
   );
 }
