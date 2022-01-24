68c16c14c5daf842011a35af2a1559626d61ff8d
Piotr Smietanka <piotrsm99@gmail.com>
2022-01-24 15:35:35 +0100

    add: data-container div

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -7,25 +7,27 @@ import { Link as Scroll, Element, animateScroll } from "react-scroll";
 export default function Home(params) {
   return (
     <Element name="home" id="section">
-      <div className="stripes_container">
-        <div className="stripe_item__1">
-          <p id="name">PIOTR ŚMIETANKA</p>
+      <div className="data-container">
+        <div className="stripes_container">
+          <div className="stripe_item__1">
+            <p id="name">PIOTR ŚMIETANKA</p>
+          </div>
+          <div className="stripe_item__2">
+            <p>Web Development</p>
+          </div>
+          <div className="stripe_item__2">
+            <p>Marketing</p>
+          </div>
+          <div className="stripe_item__2">
+            <p>Education</p>
+          </div>
         </div>
-        <div className="stripe_item__2">
-          <p>Web Development</p>
-        </div>
-        <div className="stripe_item__2">
-          <p>Marketing</p>
-        </div>
-        <div className="stripe_item__2">
-          <p>Education</p>
-        </div>
+        <img
+          className="profile_photo"
+          src="piotr_photo.png"
+          alt="Piotr Śmietanka"
+        />
       </div>
-      <img
-        className="profile_photo"
-        src="piotr_photo.png"
-        alt="Piotr Śmietanka"
-      />
     </Element>
   );
 }
