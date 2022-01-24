dcb027ae7f4ec6e78ce1671b087ec26823c26ad5
Piotr Smietanka <piotrsm99@gmail.com>
2022-01-24 15:39:11 +0100

    feat: hamburger icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,22 +6,36 @@ import { Link as Scroll, Element, animateScroll } from "react-scroll";
 export default function Navbar(params) {
   return (
     <nav className="navbar_container">
+      <div className="hamburger" />
+
+      {/* <div className="hamburger" id="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </div> */}
       <div className="navbar_left">
-        <Scroll to="home" className="home">
-          Home
-        </Scroll>
-
-        <Scroll to="about" className="about">
-          About
-        </Scroll>
-
-        <Scroll to="passions" className="passions">
-          Passions
-        </Scroll>
-
-        <Scroll to="portfolio" className="portfolio">
-          Portfolio
-        </Scroll>
+        <ul>
+          <li>
+            <Scroll to="home" className="home">
+              Home
+            </Scroll>
+          </li>
+          <li>
+            <Scroll to="about" className="about">
+              About
+            </Scroll>
+          </li>
+          <li>
+            <Scroll to="passions" className="passions">
+              Passions
+            </Scroll>
+          </li>
+          <li>
+            <Scroll to="portfolio" className="portfolio">
+              Portfolio
+            </Scroll>
+          </li>
+        </ul>
       </div>
       <div className="navbar_right">
         <Scroll to="contact">Contact Me</Scroll>
