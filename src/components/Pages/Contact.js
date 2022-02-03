1ed0d7b32b9c9f95703a48156250a763d08c1486
Piotr Smietanka <piotrsm99@gmail.com>
2022-02-03 20:18:01 +0100

    feat: add contact component

---
+++ src/components/Pages/Contact.js
@@ -0,0 +1,56 @@
+import { Element } from "react-scroll";
+
+import "./Contact.scss";
+
+export default function Contact(params) {
+  return (
+    <Element name="contact" id="section">
+      <div className="contact_container">
+        <p className="title">
+          <span>Contact</span> me!
+        </p>
+
+        <div className="contacts_container">
+          <h2>Interested to contact me?</h2>
+          <p>
+            Click <span>one</span> to open:
+          </p>
+          <div className="providers_container">
+            <a
+              href="https://github.com/petercreams/"
+              target="_blank"
+              rel="noreferrer"
+              className="provider"
+            >
+              <img src="/images/contact/Git.png" alt="" />
+            </a>
+            <a
+              href="https://www.instagram.com/peter_creams/"
+              target="_blank"
+              rel="noreferrer"
+              className="provider"
+            >
+              <img src="/images/contact/Instagram.png" alt="" />
+            </a>
+            <a
+              href="mailto:piotr_smietanka@outlook.com"
+              target="_blank"
+              rel="noreferrer"
+              className="provider"
+            >
+              <img src="/images/contact/Email.png" alt="" />
+            </a>
+            <a
+              href="tel: +48530477514"
+              target="_blank"
+              rel="noreferrer"
+              className="provider"
+            >
+              <img src="/images/contact/Telpon.png" alt="" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </Element>
+  );
+}
